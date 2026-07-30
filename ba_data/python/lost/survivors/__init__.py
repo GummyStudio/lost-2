869f8c519c46e17ab21173ab0a38db7a07960125
Mell <mellboii@tutamail.com>
2026-07-29 21:46:11 -0300

    fix bernard breaking game kys

--- ba_data/python/lost/survivors/__init__.py
+++ ba_data/python/lost/survivors/__init__.py
@@ -2,11 +2,11 @@ from lost.survivors.zoe import ZoeSurvivor
 from lost.survivors.pirate import PirateSurvivor
 from lost.survivors.mell import MellSurvivor
 from lost.survivors.kronk import KronkSurvivor
-from lost.survivors.bear import BearSurvivor
+#from lost.survivors.bear import BearSurvivor
 __all__ = [
     'ZoeSurvivor',
     'PirateSurvivor',
     'MellSurvivor',
     'KronkSurvivor',
-    'BearSurvivor',
+    #'BearSurvivor',
 ]
