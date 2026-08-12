3017fe0a3e77c8f2c413ed5e2fc30c98353fe0b8
GummyBoi_YT <150540342+GummyBoiYT1@users.noreply.github.com>
2026-08-12 02:16:41 -0400

    weakness and slowness upon going OOB to avoid abuse

--- ba_data/python/bascenev1lib/actor/spaz.py
+++ ba_data/python/bascenev1lib/actor/spaz.py
@@ -246,6 +246,7 @@ class Spaz(bs.Actor):
         self._turbo_filter_time_bucket = 0
         self._turbo_filter_counts: dict[str, int] = {}
         self.frozen = False
+        
         self.shattered = False
         self._last_hit_time: int | None = None
         self._num_times_hit = 0
@@ -1407,6 +1408,18 @@ class Spaz(bs.Actor):
                 self.handlemessage(DamageMessage(damage=(self.highlight/2)/10))
           
                 self.handlemessage(bs.StandMessage(self.getactivity().map.get_ffa_start_position([])))
+
+                # slowness and weakness
+                self.max_walk_speed *= 0.8
+
+                self.damage_scale *= 1.5
+
+                def stop():
+                    if self.exists():
+                        self.spaz.damage_scale /= 1.5
+                        self.spaz.max_walk_speed /= 0.8
+                 
+                bs.timer(5, stop)
 
 
         elif isinstance(msg, bs.StandMessage):
